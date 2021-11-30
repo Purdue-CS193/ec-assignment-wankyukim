ee3ddbaef5ef74a0acd4b477f95d1f992e57772c
Wankyu Kim <kim3656@data.cs.purdue.edu>
2021-11-29 19:50:41 -0500

    Finished Code Standards assignment

--- src/codeStandards.java
+++ src/codeStandards.java
@@ -3,7 +3,7 @@ public class codeStandards {
         /* 
          * Please correct the coding style errors in the function fixThisCode.
          * You can find a good guide to code standards here: 
-         * https://google.github.io/styleguide/javaguide.html#s4-formatting
+         * https://google.github.io/styleguidre/javaguide.html#s4-formatting
          * Look for: camel case, indentation, spacing, etc.
          */
 
@@ -14,14 +14,13 @@ public class codeStandards {
     * 
     */
     public static void fixThisCode(int num) {
-        int samenum, newnum;
-        if(num%2==0){
-            newnum=num+1; System.out.println("Provided number "+ num + " was even but is now " + newnum);
+        int sameNum, newNum;
+        if (num % 2 == 0) {
+            newNum = num + 1; 
+	    System.out.println("Provided number " + num + " was even but is now " + newNum);
+        } else {
+            sameNum = num;
+            System.out.println("yay" + sameNum + "is an odd number");
         }
-        else {
-            samenum=num;
-        System.out.println("yay"+ samenum+ "is an odd number");
-        }
-
     }
 }
